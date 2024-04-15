aa843eba37d90b36828196fe4ff05f73dd6019c1
a1x40v <a1x40v@gmail.com>
2024-04-15 17:06:56 +0300

    Implement CQRS abstractions

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,29 @@
+using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, decimal Price, string ImageUrl)
+    : ICommand<CreateProductdResult>;
+
+public record CreateProductdResult(Guid Id);
+
+internal class CreateProductCommandHandler
+    : ICommandHandler<CreateProductCommand, CreateProductdResult>
+{
+    public async Task<CreateProductdResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageUrl,
+            Price = command.Price
+        };
+
+        // save to database
+
+        return new CreateProductdResult(Guid.NewGuid());
+    }
+}
