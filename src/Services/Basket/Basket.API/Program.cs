5b1f621e0ccc81c2847a3fb3791a28791f7d8298
a1x40v <a1x40v@gmail.com>
2024-04-21 12:21:31 +0300

    Complete CRUD operations

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,9 +1,39 @@
+using Basket.API.Data;
+using BuildingBlocks.Behaviors;
+using BuildingBlocks.Exceptions.Handler;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 
+var assembly = typeof(Program).Assembly;
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+builder.Services.AddValidatorsFromAssembly(assembly);
+
+builder.Services.AddCarter();
+
+builder.Services
+    .AddMarten(options =>
+    {
+        options.Connection(builder.Configuration.GetConnectionString("Database")!);
+        options.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+    })
+    .UseLightweightSessions();
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
+
+app.MapCarter();
+app.UseExceptionHandler(opt => { });
 
 app.Run();
